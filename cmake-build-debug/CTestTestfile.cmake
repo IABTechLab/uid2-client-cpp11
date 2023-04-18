d6c9d369f478795353758f77f61f4bbf173ec2c7
Cody Constine <cody.constine@thetradedesk.com>
2023-04-18 14:20:56 -0600

    Cleaned up not needed files

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/cody.constine/repos/uid2/uid2-client-cpp11
-# Build directory: /Users/cody.constine/repos/uid2/uid2-client-cpp11/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lib")
-subdirs("app")
-subdirs("test")
